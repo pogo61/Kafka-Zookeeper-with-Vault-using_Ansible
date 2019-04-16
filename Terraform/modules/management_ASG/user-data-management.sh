b4ab71d287450b1ed0a6ca23e0792a51e5c53cd7
paulpog <paulpog@japarasolutions.com>
2019-04-16 14:00:45 +0100

    tidy up and delay management ASG build to help ensure kafka and zookeeper is running

--- Terraform/modules/management_ASG/user-data-management.sh
+++ Terraform/modules/management_ASG/user-data-management.sh
@@ -3,7 +3,7 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 echo BEGIN
 # wait for the zookeeper and kafka clusters to complete setup
 su ec2-user -c 'source ~/.bash_profile; python /tmp/install-tools/conf_tools.py'
-max=10
+max=100
 for (( i=0; i <= $max; ++i ))
 do
 echo "waiting for Kafka - $i"
@@ -11,8 +11,8 @@ if ! su ec2-user -c 'source ~/.bash_profile; ncat -vc ls zookeeper1 2181'; then
   echo "kafka not ready"
   sleep 30
 else
-  su ec2-user -c 'source ~/.bash_profile; sudo /usr/local/bin/docker-compose -f /tmp/install-tools/zoonavigator-docker-compose.yml up -d'
-  su ec2-user -c 'source ~/.bash_profile; sudo /usr/local/bin/docker-compose -f /tmp/install-tools/kafka-manager-docker-compose.yml up -d'
+  su ec2-user -c 'source ~/.bash_profile; sudo /usr/local/bin/docker-compose -f /tmp/install-tools/zoonavigator-docker-compose.yml up -d --remove-orphans'
+  su ec2-user -c 'source ~/.bash_profile; sudo /usr/local/bin/docker-compose -f /tmp/install-tools/kafka-manager-docker-compose.yml up -d --remove-orphans'
   break
 fi
 done
---
+++ Terraform/modules/management_vpc/variables.tf
@@ -0,0 +1,4 @@
+variable "kready" {
+}
+variable "zkready" {
+}
